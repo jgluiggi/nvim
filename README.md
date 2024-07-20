73d4ee3157d3bb1f30be39171feec7e04fe576da
jgluiggi <jgluiggi@protonmail.com>
2024-07-19 21:06:17 -0300

    feat: a porra toda

---
+++ README.md
@@ -0,0 +1,21 @@
+# Minha build do nvim pra não esquecer depois
+
+## Remaps:
+
+### Leader é espaço.
+
+### dingllm
+* ```<C-i>``` para consulta padrão
+* ```<C-I>``` para consulta complexa
+
+### harpoon
+
+* ```<leader>a``` para adicionar o arquivo no buffer
+* ```<C-e>``` para abrir o buffer
+* ```<C-h,j,k,l>``` para pular entre os 4 primeiros arquivos respectivamente
+
+### telescope
+
+* ```<leader>ff``` para encontrar arquivos
+* ```<leader>gf``` para catucar arquivos git
+* ```<leader>fh``` para consultar os helps do nvim
---
+++ lua/config/lazy.lua
@@ -0,0 +1,104 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+vim.opt.title = true
+vim.opt.bg=dark
+vim.opt.guicursor={a = true}
+vim.opt.mouse={a = true}
+vim.opt.hlsearch = false
+vim.opt.clipboard:append({unnamedplus = true})
+vim.opt.showmode = false
+vim.opt.ruler = false
+vim.opt.showcmd = false
+vim.opt.cursorline = true
+vim.opt.smartindent = true
+vim.opt.cc = "100"
+vim.opt.scrolloff = 8
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.compatible = false
+vim.opt.encoding = "utf-8"
+vim.opt.wildmode = { 'longest', 'list', 'full' }
+
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "S", [[:%s//g<Left><Left>]])
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+     { "LazyVim/LazyVim", dev = true, lazy = false, priority = 10000 },
+    -- import your plugins
+     { import = "plugins" },
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'dracula',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = {}
+}
