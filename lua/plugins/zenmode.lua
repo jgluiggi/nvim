543ff62b5e39d3fb3dd91d55d721106ef222f512
jgluiggi <jgluiggi@protonmail.com>
2024-08-01 20:39:37 -0300

    feat: lsp e zenmode sendo testados

---
+++ lua/plugins/zenmode.lua
@@ -0,0 +1,24 @@
+return {
+    "folke/zen-mode.nvim",
+    config = function()
+        vim.keymap.set("n", "<leader>zz", function()
+            require("zen-mode").setup {
+                window = {
+                    width = .70,
+                    options = { }
+                },
+                plugins = {
+                    options = {
+                    ruler = true,
+                    showcmd = true,
+                    laststatus = 3,
+                    }
+                },
+            }
+            require("zen-mode").toggle()
+            vim.wo.wrap = true
+            vim.wo.number = true
+            vim.wo.rnu = true
+        end)
+    end
+}
